eea51def370b65353389f7df909adb548f057120
David Jones <david.jones@affiliatewindow.com>
2017-01-14 14:59:41 +0000

    Week 1 java worksheet 1

---
+++ exercises/week01/java/worksheet-01/src/Driver.java
@@ -0,0 +1,31 @@
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
+
+        Class<BankAccount> baCls = BankAccount.class;
+
+        try {
+            BankAccount myAccount = baCls.newInstance();
+            aStorage.setValue(myAccount);
+            // Deposit
+            myAccount.deposit(15);
+
+            System.out.println( aStorage.getValue().showBalance() );
+            if( aStorage.getClass() == sStorage.getClass() ) {
+                System.out.println( "EQUAL" );
+            } else {
+                System.out.println( "NOT EQUAL" );
+            }
+        }
+        catch ( InstantiationException e ) {
+            // ...
+        }
+        catch ( IllegalAccessException e ) {
+            // ...
+        }
+    }
+}
