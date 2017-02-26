5dc8aa27e82bcb4d333f82e8c2b837916c0a1b2a
David Jones <david.jones@affiliatewindow.com>
2017-02-26 13:16:21 +0000

    CW-ONE - Added the MulInstruction class and got it working

---
+++ coursework/cw-one/src/sml/MulInstruction.java
@@ -0,0 +1,36 @@
+package sml;
+
+/**
+ * Multiplies the value in one register with a value in another register.
+ * The computed value is stored in the appointed register.
+ */
+public class MulInstruction extends Instruction {
+
+    private int result;
+    private int op1;
+    private int op2;
+
+    public MulInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    public MulInstruction(String label, int result, int op1, int op2) {
+        this(label, "mul");
+        this.result = result;
+        this.op1 = op1;
+        this.op2 = op2;
+    }
+
+    @Override
+    public void execute(Machine m) throws IllegalArgumentException {
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+
+        m.getRegisters().setRegister(result, value1 * value2);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + op1 + " * " + op2 + " to " + result;
+    }
+}
