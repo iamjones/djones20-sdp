6fe2ccb753f9b5c9a39820848da81df08bbe7072
David Jones <david.jones@affiliatewindow.com>
2017-02-26 13:25:57 +0000

    CW-ONE - Added the OutInstruction class and got it working

---
+++ coursework/cw-one/src/sml/OutInstruction.java
@@ -0,0 +1,30 @@
+package sml;
+
+/**
+ * Prints out the value of the given register to the console.
+ */
+public class OutInstruction extends Instruction {
+
+    private int register;
+
+    public OutInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    public OutInstruction(String label, int register) {
+        this(label, "out");
+        this.register = register;
+    }
+
+    @Override
+    public void execute(Machine m) throws IllegalArgumentException {
+        int value1 = m.getRegisters().getRegister(this.register);
+
+        System.out.println("Register " + this.register + " = " + value1);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " print value of " + this.register;
+    }
+}
