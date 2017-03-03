06b5ec7fc5cc3a4b57088a87e83cd5d317e03017
David Jones <david.jones@affiliatewindow.com>
2017-03-03 19:10:37 +0000

    Week 8 - Question 10

--- exercises/week08/src/main/scala/bridge/BigWheel.scala
+++ exercises/week08/src/main/scala/bridge/BigWheel.scala
@@ -1,11 +1,16 @@
 package bridge
 
-// TODO
-
 case class BigWheel(product: Product, s: String) extends Car(product, s) {
-  override def assemble: Unit = ???
 
-  override def produceProduct: Unit = ???
+    override def assemble {
+        println("Assembling " + product.productName + " for " + s)
+    }
 
-  override def printDetails: Unit = ???
+    override def produceProduct {
+        product.produce
+    }
+
+    override def printDetails {
+        println("Car: " + s + ", Product: " + product.productName)
+    }
 }
--- exercises/week08/src/main/scala/bridge/Car.scala
+++ exercises/week08/src/main/scala/bridge/Car.scala
@@ -2,9 +2,7 @@ package bridge
 
 abstract class Car(product: Product, carType: String) {
 
-  def assemble
-
-  def produceProduct
-
-  def printDetails
+    def assemble
+    def produceProduct
+    def printDetails
 }
