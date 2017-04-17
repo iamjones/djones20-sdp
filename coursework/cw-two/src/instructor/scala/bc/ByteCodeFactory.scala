c6b5a86aae35cec21bfe4acf56fbd332337049ee
David Jones <david.jones@affiliatewindow.com>
2017-04-17 13:05:24 +0100

    CW-two - Changed the magic numbers in the byte code factory to use the bytecode values

--- coursework/cw-two/src/instructor/scala/bc/ByteCodeFactory.scala
+++ coursework/cw-two/src/instructor/scala/bc/ByteCodeFactory.scala
@@ -31,24 +31,27 @@ class ByteCodeFactoryImpl extends ByteCodeFactory with ByteCodeValues {
         /**
           * Iconst requires an Int to be pushed to the stack. If one is not
           * supplied then an InvalidBytecodeException is thrown.
+          *
+          * Having to do an if statement inside each case block because we are
+          * unable to directly call the bytecode function.
           */
         byte match {
-            case 1 =>
+            case `byte` if bytecode("iconst") == byte =>
                 args match {
                     case Nil => throw new InvalidBytecodeException("Iconst byte code needs a value to be pushed to the stack")
                     case _ => Iconst(args.head)
                 }
-            case 2 => Iadd
-            case 3 => Isub
-            case 4 => Imul
-            case 5 => Idiv
-            case 6 => Irem
-            case 7 => Ineg
-            case 8 => Iinc
-            case 9 => Idec
-            case 10 => Idup
-            case 11 => Iswap
-            case 12 => Print
+            case `byte` if bytecode("iadd") == byte => Iadd
+            case `byte` if bytecode("isub") == byte => Isub
+            case `byte` if bytecode("imul") == byte => Imul
+            case `byte` if bytecode("idiv") == byte => Idiv
+            case `byte` if bytecode("irem") == byte => Irem
+            case `byte` if bytecode("ineg") == byte => Ineg
+            case `byte` if bytecode("iinc") == byte => Iinc
+            case `byte` if bytecode("idec") == byte => Idec
+            case `byte` if bytecode("idup") == byte => Idup
+            case `byte` if bytecode("iswap") == byte => Iswap
+            case `byte` if bytecode("print") == byte => Print
             case _ => throw new InvalidBytecodeException("Invalid byte code")
         }
     }
